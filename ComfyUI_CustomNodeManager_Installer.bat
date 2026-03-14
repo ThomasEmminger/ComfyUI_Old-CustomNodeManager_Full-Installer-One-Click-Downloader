5dc70f0f35795fa55c09939dfd3a36189dcd5aeb
Thomas Emminger <91199282+ThomasEmminger@users.noreply.github.com>
2026-03-14 18:11:35 +0100

    Update ComfyUI installer script with new download link
    Updated the download link for ComfyUI and added a pause at the end.

--- ComfyUI_CustomNodeManager_Installer.bat
+++ ComfyUI_CustomNodeManager_Installer.bat
@@ -5,11 +5,11 @@ timeout /t 5
 
 
 curl -L -o ComfyUI_windows_portable_nvidia_cu128.7z ^
-https://github.com/Comfy-Org/ComfyUI/releases/download/v0.9.2/ComfyUI_windows_portable_nvidia_cu128.7z
+https://github.com/Comfy-Org/ComfyUI/releases/latest/download/ComfyUI_windows_portable_nvidia_cu126.7z
 
 timeout /t 3 /nobreak >nul
 
-"C:\Program Files\7-Zip\7z.exe" x ComfyUI_windows_portable_nvidia_cu128.7z
+"C:\Program Files\7-Zip\7z.exe" x ComfyUI_windows_portable_nvidia_cu126.7z
 
 del ComfyUI_windows_portable_nvidia_cu128.7z
 
